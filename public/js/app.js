4f595afa6565b1f3813f15b8c23a90b48458ad53
blandhalen <blandhalen@blands-imac.home>
2018-08-10 16:45:24 -0400

    file setup

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+    render () {
+      return (
+          <h1> Tolkein About Books </h1>
+      )
+    }
+  }
+  
+ReactDOM.render(
+    <App />,
+    document.querySelector('main')
+  )

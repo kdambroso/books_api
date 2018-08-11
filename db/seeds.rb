f575d768d5721735eb34926f55626ac0716a5637
Jon Bonnet <jbonnet303@gmail.com>
2018-08-11 12:41:27 -0400

    pushed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# books = Book.create([{ title: 'Lord of the Rings', author: 'J.R.R Tolkien', description: 'real good book', image: 'https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg'}])

4eda6300e4e03a4844f388b3bc87d2d99c0fded4
Jon Bonnet <jbonnet303@gmail.com>
2018-08-13 16:57:19 -0400

    fixed files

--- public/js/BooksList.js
+++ public/js/BooksList.js
@@ -1,49 +1,10 @@
 class BooksList extends React.Component {
   render (){
-    console.log(this.props)
     return (
       <table>
         <tbody>
         {this.props.books.map((book, index) => {
           return (
-<<<<<<< HEAD
-          <tr>
-            <td  onClick={
-              () => {
-                this.props.toggleState('booksAvailable', 'bookAvailable');
-                this.props.getBook(book)
-                }}
-                >
-              <img src={book.image} alt={book.name}/>
-            </td>
-
-              <h3> {book.title} </h3>
-              <h3> {book.author} </h3>
-              <h3> {book.publisher} </h3>
-              <h3> {book.genre} </h3>
-
-
-            <td onClick={
-              () => {
-            this.props.toggleState('booksAvailable', 'bookAvailable');
-            this.props.getBook(book)
-            }}
-            >
-
-              <button className='button is-warning is-small'
-             onClick={() =>this.props.handleUpdateSubmit(book)} >Edit</button>
-            </td>
-            <td>
-              <button className='button is-danger is-small'
-              onClick={()=>this.props.deleteBook(book, index)}
-              >
-              Delete
-              </button>
-            </td>
-          </tr>
-        )
-=======
->>>>>>> 7ec65396bb5f5f53a6e455b5f6d65bd221d89851
             <tr>
               <td onClick={()=>
               { this.props.getBook(book); this.props.toggleState('booksListAvailable', 'bookAvailable')}

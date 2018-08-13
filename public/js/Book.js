d6d8a1e77f50bdc61b76b107aa37fcb1db84019d
Kellie Dambroso <kelliedambroso@Kellies-MacBook-Pro.local>
2018-08-13 09:22:38 -0500

    can edit

--- public/js/Book.js
+++ public/js/Book.js
@@ -1,32 +1,32 @@
 class Book extends React.Component {
 
-    render () {
-      return (
-        <div>
-          <div className='tile is-ancestor'>
-            <div className='tile is-2'>
-              <div>
-                <img src={this.props.book.image} alt={this.props.book.name} />
-              </div>
+  render () {
+    return (
+      <div>
+        <div className='tile is-ancestor'>
+          <div className='tile is-2'>
+            <div>
+              <img src={this.props.book.image} alt={this.props.book.title} />
             </div>
-            <div className='tile is-2'></div>
-            <div className='tile'>
-              <div>
-                <h3 className='tile is-child box'><span>Title:</span> {this.props.book.title} </h3>
-                <p className='tile is-child box'><span>Author:</span> {this.props.book.author} </p>
-                <p className='tile is-child box'><span>Publisher:</span> {this.props.book.publisher} </p>
-                <p className='tile is-child box'><span>Genre:</span>{this.props.book.genre} </p>
-              </div>
-
-              <div className='tile'>
-              </div>
-            <div className='tile'>
-              <button className='button is-warning' onClick={()=> this.props.toggleState('booksListAvailable', 'bookAvailable')}>See All Books</button>
-            </div>
-            </div>
-            </div>
-            <BookForm Book={this.props.Book}   handleSubmit={this.props.handleSubmit}/>
           </div>
-        )
-    }
+          <div className='tile is-2'></div>
+          <div className='tile'>
+            <div>
+              <h3 className='tile is-child box'><span>Name:</span> {this.props.book.title} </h3>
+              <p className='tile is-child box'><span>Phone:</span> {this.props.book.publisher} </p>
+              <p className='tile is-child box'><span>Key Skill:</span> {this.props.book.genre} </p>
+              <p className='tile is-child box'><span>Age:</span>{this.props.book.author} </p>
+
+            </div>
+            <div className='tile'>
+            </div>
+          <div className='tile'>
+            <button className='button is-warning' onClick={()=> this.props.toggleState('booksListAvailable', 'bookAvailable')}>See Full List</button>
+          </div>
+          </div>
+        </div>
+        <BookForm book={this.props.book}   handleSubmit={this.props.handleSubmit}/>
+      </div>
+    )
   }
+}
--- public/js/Books.js
+++ public/js/Books.js
@@ -1,143 +1,145 @@
 class Books extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      booksListAvailable: true,
-      addBookAvailable: false,
-      bookAvailable: false,
-      editBookAvailable: false,
-      books:[],
-      book: {}
+  constructor (props){
+  super(props)
+  this.state = {
+    booksListAvailable: true,
+    addBookAvailable: false,
+    bookAvailable: false,
+    editBookAvailable: false,
+    books : [],
+    book: {}
     }
-      this.toggleState = this.toggleState.bind(this)
-      this.getBook = this.getBook.bind(this)
-      this.handleCreate = this.handleCreate.bind(this)
-      this.handleSubmit = this.handleSubmit.bind(this)
-      this.deleteBook = this.deleteBook.bind(this)
-      this.handleUpdateSubmit = this.handleUpdateSubmit.bind(this)
+    this.toggleState = this.toggleState.bind(this)
+    this.getBook = this.getBook.bind(this)
+    this.handleCreate = this.handleCreate.bind(this)
+    this.handleCreateSubmit = this.handleCreateSubmit.bind(this)
+    this.deleteBook = this.deleteBook.bind(this)
+    this.handleUpdateSubmit= this.handleUpdateSubmit.bind(this)
+
+
+
   }
+
   componentDidMount () {
-    this.getBooks()
+    this.getBooks();
   }
-  handleCreate (book){
-    console.log([book, ...this.state.books])
-    this.setState({books:[book, ...this.state.books]})
-  }
-  handleSubmit (book) {
-  fetch('/books', {
-    body: JSON.stringify(book),
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json, text/plain, */*',
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(createdBook => {
-    return createdBook.json()
-  })
-  .then(jsonedBook => {
-    this.handleCreate(jsonedBook)
-    this.toggleState('addBookAvailable',
-     'booksListAvailable')
-  })
-  .catch(error => console.log(error))
-}
-handleUpdateSubmit(book) {
-  fetch('/books/' + book.id,{
-    body:JSON.stringify(book),
-    method: 'PUT',
-    headers: {
-      'Accept': 'application/json, text/plain, */*',
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(updatedBook => {
-    return updatedBook.json()
-  })
-  .then(jsonedBook => {
-    this.getBooks()
-    this.toggleState('booksListAvailable',
-     'bookAvailable')
-  })
-  .catch(error => console.log(error))
-}
 
-  deleteBook(book, index) {
+  deleteBook (book, index) {
     fetch('books/' + book.id,
-  {
-    method: 'DELETE'
-  })
-  .then(data => {
-    this.setState({
-      books: [
-        ...this.state.books.slice(0, index),
-        ...this.state.books.slice(index + 1)
-      ]
-    })
-  })
-  }
-
-  getBooks() {
-    fetch('/books')
-      .then(response => response.json())
-      .then(JSONdata => {
+      {
+        method: 'DELETE'
+      })
+      .then(data => {
         this.setState({
-          books: JSONdata
+          books: [
+            ...this.state.books.slice(0, index),
+            ...this.state.books.slice(index + 1)
+          ]
         })
       })
-      .catch(error => console.log(error))
   }
 
-  getBook(book){
-    this.setState({
-      book: book
-    })
+  handleCreate (book) {
+    console.log([book, ...this.state.books])
+    this.setState({books: [book, ...this.state.books]})
   }
+  handleUpdateSubmit (book) {
+      fetch('/books/'+ book.id, {
+        body: JSON.stringify(book),
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(updatedBook => {
+          return updatedBook.json()
+        })
+        .then(jsonedBook => {
+          //need to update state be naughty, call that db!
+          this.getBooks()
+          this.toggleState('booksListAvailable', 'bookAvailable')
+        })
+        .catch(error => console.log(error))
+
+  }
+
+
+
+  handleCreateSubmit (book) {
+    fetch('/books', {
+      body: JSON.stringify(book),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(createdBook => {
+        return createdBook.json()
+      })
+      .then(jsonedBook => {
+        this.handleCreate(jsonedBook)
+        this.toggleState('addBookAvailable', 'booksListAvailable')
+      })
+      .catch(error => console.log(error))
+}
+
+  getBook( book ) {
+    this.setState({book: book})
+  }
+
+  getBooks () {
+    fetch('/books')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          books: data
+        })
+      }).catch(error => console.log(error))
+  }
+
+
 
   toggleState (st1, st2) {
     this.setState({
       [st1]: !this.state[st1],
       [st2]: !this.state[st2]
     })
-}
+  }
 
 
   render () {
-    console.log(this.state);
     return (
       <div className='books column'>
         <h2> Books </h2>
-{this.state.booksListAvailable ?
-        <button
-          className='button is-success'
-            onClick={()=>this.toggleState('addBookAvailable', 'booksListAvailable')}
-              >Add a Book</button>:''}
-        {this.state.booksListAvailable
-          ? <BooksList
-          toggleState={this.toggleState}
-          books={this.state.books}
-          getBook={this.getBook}
-          deleteBook={this.deleteBook}
-          /> : ''
+        {this.state.booksListAvailable ? <button className='button is-success' onClick={()=>this.toggleState('addBookAvailable', 'booksListAvailable')}>Add a Book</button> :''}
+        {
+          this.state.booksListAvailable ?
+            <BooksList
+             toggleState={this.toggleState}
+             books={this.state.books}
+             getBook={this.getBook}
+             deleteBook={this.deleteBook}
+            /> : ''
         }
-        {this.state.addBookAvailable
-          ? <BookForm
+        {
+          this.state.addBookAvailable ?
+           <BookForm
             toggleState={this.toggleState}
             handleCreate={this.handleCreate}
-            handleSubmit={this.handleSubmit}
-          />
-         : ''
-       }
-        {this.state.bookAvailable
-          ? <Book
-            toggleState={this.toggleState}
-            book={this.state.book}
-            handleSubmit={this.handleUpdateSubmit}
-         />
-         : ''
-       }
-    </div>
-
+            handleSubmit={this.handleCreateSubmit}
+           /> : ''
+         }
+         {
+   this.state.bookAvailable ?
+    <Book
+     toggleState={this.toggleState}
+     book={this.state.book}
+     handleSubmit={this.handleUpdateSubmit}
+    /> : ''
+ }
+      </div>
     )
   }
 }

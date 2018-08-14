6b264a9e9e8c9e324feef4eb730a0bb4585b7474
blandhalen <blandhalen@blands-imac.home>
2018-08-14 15:01:39 -0400

    user model route controller functions

---
+++ app/models/user.rb
@@ -0,0 +1,59 @@
+class User
+    DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'books_api_development'})
+
+    def self.all
+        result = DB.exec("SELECT * FROM users;")
+        return result.map do |result|
+            {
+                "id" => result["id"].to_i,
+                "username" => result["username"], 
+                "pass_word" => result["pass_word"]
+            }
+        end
+    end
+
+    def self.find(id)
+        results = DB.exec("SELECT * FROM users WHERE id=#{id};")
+        return {
+            "id" => results.first["id"].to_i, 
+            "username" => results.first["username"], 
+            "pass_word" => results.first["pass_word"]
+        }
+    end
+
+    def self.create(opts)
+        results = DB.exec(
+            <<-SQL
+                INSERT INTO users (username, pass_word)
+                VALUES ( '#{opts["username"]}', '#{opts["pass_word"]}')
+                RETURNING id, username, pass_word;
+            SQL
+        )
+        return {
+            "id" => results.first["id"].to_i, 
+            "username" => results.first["username"], 
+            "pass_word" => results.first["pass_word"]
+        }
+    end
+
+    def self.delete(id)
+        results = DB.exec("DELETE FROM users WHERE id=#{id};")
+        return { "deleted" => true }
+    end
+
+    def self.update(id, opts)
+        results = DB.exec(
+            <<-SQL
+                UPDATE users
+                SET username='#{opts["username"]}', pass_word='#{opts["pass_word"]}'
+                WHERE id=#{id}
+                RETURNING id, username, pass_word; 
+            SQL
+        )
+        return {
+            "id" => results.first["id"].to_i, 
+            "username" => results.first["username"], 
+            "pass_word" => results.first["pass_word"]
+        }
+    end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   delete '/books/:id', to: 'books#delete'
   put '/books/:id', to: 'books#update'
 
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  delete '/users/:id', to: 'users#delete'
+  put '/users/:id', to: 'users#update'
+
 end

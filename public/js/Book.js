f3d3c0490397dab33f513579abf8c0e99754b79d
Jon Bonnet <jbonnet303@gmail.com>
2018-08-15 17:04:15 -0400

    added css

--- public/js/Book.js
+++ public/js/Book.js
@@ -3,28 +3,20 @@ class Book extends React.Component {
     return (
       <div>
         <div className='tile is-ancestor'>
+        <div className='tile'>
+          <button className='button is-warning' onClick={()=> this.props.toggleState('booksListAvailable', 'bookAvailable')}>See Full List</button>
+        </div>
           <div className='tile is-2'>
             <div>
               <img src={this.props.book.image} alt={this.props.book.title} />
             </div>
           </div>
           <div className='tile is-2'></div>
+          </div>
           <div className='tile'>
             <div>
-              <h3 className='tile is-child box'><span>Title:</span> {this.props.book.title} </h3>
-              <p className='tile is-child box'><span>Author:</span>{this.props.book.author} </p>
-              <p className='tile is-child box'><span>Summary:</span>{this.props.book.summary} </p>
-              <p className='tile is-child box'><span>Genre:</span> {this.props.book.genre} </p>
-              <p className='tile is-child box'><span>Publisher:</span> {this.props.book.publisher} </p>
-
-             
-
-            </div>
             <div className='tile'>
             </div>
-          <div className='tile'>
-            <button className='button is-warning' onClick={()=> this.props.toggleState('booksListAvailable', 'bookAvailable')}>See Full List</button>
-          </div>
           </div>
         </div>
         <BookForm book={this.props.book}   handleSubmit={this.props.handleSubmit}/>
@@ -34,3 +26,10 @@ class Book extends React.Component {
     )
   }
 }
+
+
+// <h3 className='tile is-child box'><span>Title:</span> {this.props.book.title} </h3>
+// <p className='tile is-child box'><span>Author:</span>{this.props.book.author} </p>
+// <p className='tile is-child box'><span>Summary:</span>{this.props.book.summary} </p>
+// <p className='tile is-child box'><span>Genre:</span> {this.props.book.genre} </p>
+// <p className='tile is-child box'><span>Publisher:</span> {this.props.book.publisher} </p>

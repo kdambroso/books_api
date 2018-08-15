d31600829a0b60e21caa273a6e608211751ec879
Kellie Dambroso <kelliedambroso@Kellies-MacBook-Pro.local>
2018-08-15 18:36:12 -0500

    username

--- public/js/Book.js
+++ public/js/Book.js
@@ -1,5 +1,42 @@
 class Book extends React.Component {
+  render () {
+    return (
+      <div>
+        <div className='tile is-ancestor'>
+          <div className='tile is-2'>
+            <div>
+              <img src={this.props.book.image} alt={this.props.book.title} />
+            </div>
+          </div>
+          <div className='tile is-2'></div>
+          <div className='tile'>
+            {/*<div>
+              <h3 className='tile is-child box'><span>Title:</span> {this.props.book.title} </h3>
+              <p className='tile is-child box'><span>Author:</span>{this.props.book.author} </p>
+              <p className='tile is-child box'><span>Genre:</span> {this.props.book.genre} </p>
+              <p className='tile is-child box'><span>Publisher:</span> {this.props.book.publisher} </p>
+              <p className='tile is-child box'><span>Summary:</span>{this.props.book.summary} </p>
+            </div>*/}
+            <div className='tile'>
+            </div>
+          <div className='tile'>
+            <button className='button is-warning' onClick={()=> this.props.toggleState('booksListAvailable', 'bookAvailable')}>See Full List</button>
+          </div>
+          </div>
+        </div>
+        <BookForm book={this.props.book}   handleSubmit={this.props.handleSubmit}/>
+        <div><button className="button is-link" onClick={()=> this.props.toggleState('booksListAvailable', 'bookAvailable')}>Cancel</button>
+        </div>
 
+      </div>
+    )
+  }
+}
+
+
+
+
+class BookShow extends React.Component {
   render () {
     return (
       <div>
@@ -12,27 +49,22 @@ class Book extends React.Component {
           <div className='tile is-2'></div>
           <div className='tile'>
             <div>
-              {/* <h3 className='tile is-child box'><span>Title:</span> {this.props.book.title} </h3>
-              <p className='tile is-child box'><span>Author:</span>{this.props.book.author} </p>
-              <p className='tile is-child box'><span>Summary:</span>{this.props.book.summary} </p>
-              <p className='tile is-child box'><span>Genre:</span> {this.props.book.genre} </p>
-              <p className='tile is-child box'><span>Publisher:</span> {this.props.book.publisher} </p> */}
-
-             
+              <h3 className='tile '><span>Title:</span> {this.props.book.title} </h3>
+              <p className='tile is-child '><span>Author:</span>{this.props.book.author} </p>
+              <p className='tile is-child '><span>Genre:</span> {this.props.book.genre} </p>
+              <p className='tile is-child '><span>Publisher:</span> {this.props.book.publisher} </p>
+              <p className='tile is-child '><span>Summary:</span>{this.props.book.summary} </p>
 
             </div>
             <div className='tile'>
             </div>
           <div className='tile'>
-            <button className='button is-warning' onClick={()=> this.props.toggleState('booksListAvailable', 'bookAvailable')}>See Full List</button>
-          </div>
+            <button className='button is-warning' onClick={()=> this.props.toggleState('bookShowAvailable', 'booksListAvailable')}>See Full List</button>
+
           </div>
         </div>
-        <BookForm book={this.props.book}   handleSubmit={this.props.handleSubmit}/>
 
-        <div><button className="button is-link" onClick={()=> this.props.toggleState('booksListAvailable', 'bookAvailable')}>Cancel</button>
         </div>
-
       </div>
     )
   }
